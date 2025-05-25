b1b6b01d6f4295b9cb5045de1b5b6dece1ad7d0d
Kasper Nyhus <kanyhus@gmail.com>
2025-05-25 14:13:28 +0200

    use f32 instead of f64

--- src/meter.rs
+++ src/meter.rs
@@ -8,46 +8,48 @@ use ratatui::{
     widgets::{Paragraph, StatefulWidget},
 };
 
-const MIN_DB: f64 = -120.0;
-const YELLOW_START_DB: f64 = -12.0;
-const RED_START_DB: f64 = -3.0;
+const MIN_DB: f32 = -120.0;
+const YELLOW_START_DB: f32 = -12.0;
+const RED_START_DB: f32 = -3.0;
 
 /// A helper struct to convert between decibels and ratios for metering.
 struct MeterScale {}
 
 impl MeterScale {
-    const METER_LOG_SCALE_FACTOR: f64 = 2.0;
+    /// Determines the scale factor for the meter's logarithmic transformation.
+    /// This factor is used to increase the resolution of the meter at higher dB values.
+    const METER_LOG_SCALE_FACTOR: f32 = 2.0;
 
     /// Convert a decibel value to a ratio and apply an exponential transformation
     /// to increase meter resolution at higher db values
-    pub fn db_to_ratio(db: f64) -> f64 {
-        let db_ratio = 10_f64.powf(db / 20.0);
-        let min_db_ratio = 10_f64.powf(MIN_DB / 20.0);
+    pub fn db_to_ratio(db: f32) -> f32 {
+        let db_ratio = 10_f32.powf(db / 20.0);
+        let min_db_ratio = 10_f32.powf(MIN_DB / 20.0);
         let linear_ratio = (db_ratio.log10() - min_db_ratio.log10()) / (0.0 - min_db_ratio.log10());
         linear_ratio.powf(Self::METER_LOG_SCALE_FACTOR)
     }
 
     /// Convert a ratio to a decibel value and inverse the exponential transformation
-    pub fn ratio_to_db(ratio: f64) -> f64 {
+    pub fn ratio_to_db(ratio: f32) -> f32 {
         let linear_ratio = ratio.powf(1.0 / Self::METER_LOG_SCALE_FACTOR);
-        let min_db_ratio = 10_f64.powf(MIN_DB / 20.0);
+        let min_db_ratio = 10_f32.powf(MIN_DB / 20.0);
         let db_ratio =
-            10_f64.powf(linear_ratio * (0.0 - min_db_ratio.log10()) + min_db_ratio.log10());
+            10_f32.powf(linear_ratio * (0.0 - min_db_ratio.log10()) + min_db_ratio.log10());
         20.0 * db_ratio.log10()
     }
 }
 
 lazy_static! {
-    static ref YELLOW_START: f64 = MeterScale::db_to_ratio(self::YELLOW_START_DB);
-    static ref RED_START: f64 = MeterScale::db_to_ratio(RED_START_DB);
-    static ref LABEL_60: f64 = MeterScale::db_to_ratio(-60.0);
-    static ref LABEL_40: f64 = MeterScale::db_to_ratio(-40.0);
-    static ref LABEL_30: f64 = MeterScale::db_to_ratio(-30.0);
-    static ref LABEL_24: f64 = MeterScale::db_to_ratio(-24.0);
-    static ref LABEL_12: f64 = MeterScale::db_to_ratio(-12.0);
-    static ref LABEL_6: f64 = MeterScale::db_to_ratio(-6.0);
-    static ref LABEL_3: f64 = MeterScale::db_to_ratio(-3.0);
-    static ref LABEL_0: f64 = MeterScale::db_to_ratio(-0.0);
+    static ref YELLOW_START: f32 = MeterScale::db_to_ratio(self::YELLOW_START_DB);
+    static ref RED_START: f32 = MeterScale::db_to_ratio(RED_START_DB);
+    static ref LABEL_60: f32 = MeterScale::db_to_ratio(-60.0);
+    static ref LABEL_40: f32 = MeterScale::db_to_ratio(-40.0);
+    static ref LABEL_30: f32 = MeterScale::db_to_ratio(-30.0);
+    static ref LABEL_24: f32 = MeterScale::db_to_ratio(-24.0);
+    static ref LABEL_12: f32 = MeterScale::db_to_ratio(-12.0);
+    static ref LABEL_6: f32 = MeterScale::db_to_ratio(-6.0);
+    static ref LABEL_3: f32 = MeterScale::db_to_ratio(-3.0);
+    static ref LABEL_0: f32 = MeterScale::db_to_ratio(-0.0);
 }
 
 /// A widget to display an audio meter.
@@ -60,7 +62,7 @@ lazy_static! {
 /// the meter to hold its peak value for a certain amount of time.
 #[derive(Debug, Default, Clone, PartialEq)]
 pub struct Meter {
-    ratio: f64,
+    ratio: f32,
 }
 
 /// State of the [`Meter`] widget
@@ -75,7 +77,7 @@ pub struct Meter {
 /// - [`last_peak_time`]: the time when the peak value was last updated
 #[derive(Debug, Clone)]
 pub struct MeterState {
-    pub peak_hold_ratio: f64,
+    pub peak_hold_ratio: f32,
     pub peak_hold_time: Duration,
     pub last_peak_time: Instant,
 }
@@ -96,18 +98,15 @@ impl Meter {
         Self { ratio: 0.0 }
     }
 
-    /// Set the value of the [`Meter`] widget in decibels.
+    /// Set the value of the [`Meter`] widget in decibels relative to full scale.
     ///
-    /// # Panics
-    ///
-    /// This method will panic if the value of `db` is above 0.0.
+    /// This method will saturate values above 0.0.
     #[must_use = "method moves the value of self and returns the modified value"]
-    pub fn db(mut self, db: f64) -> Self {
-        assert!(db <= 0.0, "dB value cannot be above 0.0.");
-        if db < MIN_DB {
-            self.ratio = 0.0;
+    pub fn db(mut self, dbfs: f32) -> Self {
+        if (MIN_DB..=0.0).contains(&dbfs) {
+            self.ratio = MeterScale::db_to_ratio(dbfs);
         } else {
-            self.ratio = MeterScale::db_to_ratio(db);
+            self.ratio = 0.0;
         }
         self
     }
@@ -120,7 +119,7 @@ impl Meter {
     ///
     /// This method will panic if the value of `ratio` is not between 0.0 and 1.0 inclusively.
     #[must_use = "method moves the value of self and returns the modified value"]
-    pub fn ratio(mut self, ratio: f64) -> Self {
+    pub fn ratio(mut self, ratio: f32) -> Self {
         assert!(
             (0.0..=1.0).contains(&ratio),
             "Ratio should be between 0 and 1 inclusively."
@@ -155,8 +154,8 @@ impl Meter {
                 .areas(area);
 
         // Compute the start of the yellow and red zones
-        let yellow_start = area.left() + (area.width as f64 * *YELLOW_START).round() as u16;
-        let red_start = area.left() + (area.width as f64 * *RED_START).round() as u16;
+        let yellow_start = area.left() + (area.width as f32 * *YELLOW_START).round() as u16;
+        let red_start = area.left() + (area.width as f32 * *RED_START).round() as u16;
         let end = area.left() + area.width;
 
         // Update the peak hold value
@@ -165,16 +164,16 @@ impl Meter {
             state.peak_hold_ratio = self.ratio;
             state.last_peak_time = Instant::now();
         } else if elapsed.as_secs() > state.peak_hold_time.as_secs() {
-            state.peak_hold_ratio *= (0.99 - 0.01 * elapsed.as_secs_f64()).clamp(0.1, 0.99);
+            state.peak_hold_ratio *= (0.99 - 0.01 * elapsed.as_secs_f32()).clamp(0.1, 0.99);
         }
 
         let peak_x =
-            meter_area.left() + (f64::from(area.width) * state.peak_hold_ratio).round() as u16;
+            meter_area.left() + (f32::from(area.width) * state.peak_hold_ratio).round() as u16;
 
         // Render the meter
         for y in meter_area.top()..meter_area.bottom() {
             for x in meter_area.left()..end {
-                if x <= meter_area.left() + (f64::from(area.width) * self.ratio).round() as u16 {
+                if x <= meter_area.left() + (f32::from(area.width) * self.ratio).round() as u16 {
                     buf[(x, y)]
                         .set_symbol(symbols::block::SEVEN_EIGHTHS)
                         .set_fg(self.get_color(x, yellow_start, red_start));
@@ -232,23 +231,23 @@ impl Meter {
     }
 
     // Helper functions to render individual labels
-    fn render_label(&self, text: &str, ratio: f64, label_area: Rect, buf: &mut Buffer) {
+    fn render_label(&self, text: &str, ratio: f32, label_area: Rect, buf: &mut Buffer) {
         self.render_label_offset(text, ratio, label_area, buf, text.len() as u16, true);
     }
 
     fn render_label_offset(
         &self,
         text: &str,
-        ratio: f64,
+        ratio: f32,
         label_area: Rect,
         buf: &mut Buffer,
         label_width: u16,
         offset: bool,
     ) {
         let x = if offset {
-            label_area.left() + (label_area.width as f64 * ratio).round() as u16 - label_width / 2
+            label_area.left() + (label_area.width as f32 * ratio).round() as u16 - label_width / 2
         } else {
-            label_area.left() + (label_area.width as f64 * ratio).round() as u16
+            label_area.left() + (label_area.width as f32 * ratio).round() as u16
         };
 
         Paragraph::new(text).render(
@@ -278,9 +277,9 @@ mod tests {
     use super::*;
 
     #[test]
-    #[should_panic = "dB value cannot be above 0.0."]
     fn meter_invalid_db_upper_bound() {
-        let _ = Meter::default().db(0.1);
+        let meter = Meter::default().db(0.1);
+        assert_eq!(meter.ratio, 0.0)
     }
 
     #[test]
